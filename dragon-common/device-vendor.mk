fccb84b9ed926f9bbec82a847ee83c53ca21ca09
followmsi <followmsi@gmail.com>
2018-08-17 18:45:54 +0200

    dragon-p

---
+++ dragon-common/device-vendor.mk
@@ -0,0 +1,159 @@
+# Copyright 2015-2018 The Dirty Unicorns Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Proprietary Nvidia blob(s) necessary for Dragon-common hardware
+PRODUCT_COPY_FILES := \
+    vendor/google/dragon-common/proprietary/vendor/etc/model_frontal.xml:vendor/etc/model_frontal.xml \
+    vendor/google/dragon-common/proprietary/vendor/etc/version_info.txt:vendor/etc/version_info.txt \
+    vendor/google/dragon-common/proprietary/vendor/etc/selinux/nonplat_mac_permissions.xml:vendor/etc/selinux/nonplat_mac_permissions.xml \
+    vendor/google/dragon-common/proprietary/vendor/firmware/cros-pd/dingdong_v1.7.317-b0bb7c9.bin:root/vendor/firmware/cros-pd/dingdong_v1.7.317-b0bb7c9.bin \
+    vendor/google/dragon-common/proprietary/vendor/firmware/cros-pd/dingdong_v1.7.575-96b74f1.bin:root/vendor/firmware/cros-pd/dingdong_v1.7.575-96b74f1.bin \
+    vendor/google/dragon-common/proprietary/vendor/firmware/cros-pd/dingdong_v1.7.684-69498dd.bin:root/vendor/firmware/cros-pd/dingdong_v1.7.684-69498dd.bin \
+    vendor/google/dragon-common/proprietary/vendor/firmware/cros-pd/hoho_v1.7.317-b0bb7c9.bin:root/vendor/firmware/cros-pd/hoho_v1.7.317-b0bb7c9.bin \
+    vendor/google/dragon-common/proprietary/vendor/firmware/cros-pd/hoho_v1.7.575-96b74f1.bin:root/vendor/firmware/cros-pd/hoho_v1.7.575-96b74f1.bin \
+    vendor/google/dragon-common/proprietary/vendor/firmware/cros-pd/hoho_v1.7.684-69498dd.bin:root/vendor/firmware/cros-pd/hoho_v1.7.684-69498dd.bin \
+    vendor/google/dragon-common/proprietary/vendor/firmware/cros-pd/zinger_v1.7.509-e5bffd3.bin:root/vendor/firmware/cros-pd/zinger_v1.7.509-e5bffd3.bin \
+    vendor/google/dragon-common/proprietary/vendor/firmware/cros-pd/zinger_v1.7.539-91a0fa2.bin:root/vendor/firmware/cros-pd/zinger_v1.7.539-91a0fa2.bin \
+    vendor/google/dragon-common/proprietary/vendor/firmware/nouveau/acr_ucode.bin:root/vendor/firmware/nouveau/acr_ucode.bin \
+    vendor/google/dragon-common/proprietary/vendor/firmware/nouveau/fecs.bin:root/vendor/firmware/nouveau/fecs.bin \
+    vendor/google/dragon-common/proprietary/vendor/firmware/nouveau/fecs_sig.bin:root/vendor/firmware/nouveau/fecs_sig.bin \
+    vendor/google/dragon-common/proprietary/vendor/firmware/nouveau/gpmu_ucode_desc.bin:root/vendor/firmware/nouveau/gpmu_ucode_desc.bin \
+    vendor/google/dragon-common/proprietary/vendor/firmware/nouveau/gpmu_ucode_image.bin:root/vendor/firmware/nouveau/gpmu_ucode_image.bin \
+    vendor/google/dragon-common/proprietary/vendor/firmware/nouveau/nv12b_bundle:root/vendor/firmware/nouveau/nv12b_bundle \
+    vendor/google/dragon-common/proprietary/vendor/firmware/nouveau/nv12b_fuc41ac:root/vendor/firmware/nouveau/nv12b_fuc41ac \
+    vendor/google/dragon-common/proprietary/vendor/firmware/nouveau/nv12b_fuc41ad:root/vendor/firmware/nouveau/nv12b_fuc41ad \
+    vendor/google/dragon-common/proprietary/vendor/firmware/nouveau/nv12b_fuc409c:root/vendor/firmware/nouveau/nv12b_fuc409c \
+    vendor/google/dragon-common/proprietary/vendor/firmware/nouveau/nv12b_fuc409d:root/vendor/firmware/nouveau/nv12b_fuc409d \
+    vendor/google/dragon-common/proprietary/vendor/firmware/nouveau/nv12b_method:root/vendor/firmware/nouveau/nv12b_method \
+    vendor/google/dragon-common/proprietary/vendor/firmware/nouveau/nv12b_sw_ctx:root/vendor/firmware/nouveau/nv12b_sw_ctx \
+    vendor/google/dragon-common/proprietary/vendor/firmware/nouveau/nv12b_sw_nonctx:root/vendor/firmware/nouveau/nv12b_sw_nonctx \
+    vendor/google/dragon-common/proprietary/vendor/firmware/nouveau/pmu_bl.bin:root/vendor/firmware/nouveau/pmu_bl.bin \
+    vendor/google/dragon-common/proprietary/vendor/firmware/nouveau/pmu_sig.bin:root/vendor/firmware/nouveau/pmu_sig.bin \
+    vendor/google/dragon-common/proprietary/vendor/firmware/nvidia/tegra210/bpmp.bin:root/vendor/firmware/nvidia/tegra210/bpmp.bin \
+    vendor/google/dragon-common/proprietary/vendor/firmware/nvidia/tegra210/nvdec_bl_prod.bin:root/vendor/firmware/nvidia/tegra210/nvdec_bl_prod.bin \
+    vendor/google/dragon-common/proprietary/vendor/firmware/nvidia/tegra210/nvdec_ns.bin:root/vendor/firmware/nvidia/tegra210/nvdec_ns.bin \
+    vendor/google/dragon-common/proprietary/vendor/firmware/nvidia/tegra210/nvdec_prod.bin:root/vendor/firmware/nvidia/tegra210/nvdec_prod.bin \
+    vendor/google/dragon-common/proprietary/vendor/firmware/nvidia/tegra210/nvenc.bin:root/vendor/firmware/nvidia/tegra210/nvenc.bin \
+    vendor/google/dragon-common/proprietary/vendor/firmware/nvidia/tegra210/nvjpg.bin:root/vendor/firmware/nvidia/tegra210/nvjpg.bin \
+    vendor/google/dragon-common/proprietary/vendor/firmware/nvidia/tegra210/tegra_lp0_resume.fw:root/vendor/firmware/nvidia/tegra210/tegra_lp0_resume.fw \
+    vendor/google/dragon-common/proprietary/vendor/firmware/nvidia/tegra210/vic.bin:root/vendor/firmware/nvidia/tegra210/vic.bin \
+    vendor/google/dragon-common/proprietary/vendor/firmware/nvidia/tegra210/xusb.bin:root/vendor/firmware/nvidia/tegra210/xusb.bin \
+    vendor/google/dragon-common/proprietary/vendor/firmware/bcm4350c0.hcd:vendor/firmware/bcm4350c0.hcd \
+    vendor/google/dragon-common/proprietary/vendor/firmware/fw_bcmdhd.bin:vendor/firmware/fw_bcmdhd.bin \
+    vendor/google/dragon-common/proprietary/vendor/firmware/fw_bcmdhd_apsta.bin:vendor/firmware/fw_bcmdhd_apsta.bin \
+    vendor/google/dragon-common/proprietary/vendor/firmware/synaptics.img:vendor/firmware/synaptics.img \
+    vendor/google/dragon-common/proprietary/vendor/lib/egl/libEGL_tegra.so:vendor/lib/egl/libEGL_tegra.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/egl/libGLESv1_CM_tegra.so:vendor/lib/egl/libGLESv1_CM_tegra.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/egl/libGLESv2_tegra.so:vendor/lib/egl/libGLESv2_tegra.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/hw/audio.primary.tegra.so:vendor/lib/hw/audio.primary.tegra.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/hw/camera.dragon.so:vendor/lib/hw/camera.dragon.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/hw/gatekeeper.dragon.so:vendor/lib/hw/gatekeeper.dragon.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/hw/gralloc.tegra.so:vendor/lib/hw/gralloc.tegra.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/hw/keystore.dragon.so:vendor/lib/hw/keystore.dragon.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/hw/memtrack.dragon.so:vendor/lib/hw/memtrack.dragon.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/hw/ts.default.so:vendor/lib/hw/ts.default.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/hw/vulkan.tegra.so:vendor/lib/hw/vulkan.tegra.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:vendor/lib/mediadrm/libwvdrmengine.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libdrm.so:vendor/lib/libdrm.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libfcamdng.so:vendor/lib/libfcamdng.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libglcore.so:vendor/lib/libglcore.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnppc.so:vendor/lib/libnppc.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnppi.so:vendor/lib/libnppi.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvaudiofx.so:vendor/lib/libnvaudiofx.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvavp.so:vendor/lib/libnvavp.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvblit.so:vendor/lib/libnvblit.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvcam_imageencoder.so:vendor/lib/libnvcam_imageencoder.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvcamerahdr_v3.so:vendor/lib/libnvcamerahdr_v3.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvcameratools.so:vendor/lib/libnvcameratools.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvcms.so:vendor/lib/libnvcms.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvcompute.so:vendor/lib/libnvcompute.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvddk_2d_v2.so:vendor/lib/libnvddk_2d_v2.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvddk_vic.so:vendor/lib/libnvddk_vic.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvglsi.so:vendor/lib/libnvglsi.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvgr.so:vendor/lib/libnvgr.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvisp_v3.so:vendor/lib/libnvisp_v3.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvme_msenc.so:vendor/lib/libnvme_msenc.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvmm.so:vendor/lib/libnvmm.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvmm_asfparser.so:vendor/lib/libnvmm_asfparser.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvmm_audio.so:vendor/lib/libnvmm_audio.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvmm_aviparser.so:vendor/lib/libnvmm_aviparser.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvmm_camera_v3.so:vendor/lib/libnvmm_camera_v3.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvmm_contentpipe.so:vendor/lib/libnvmm_contentpipe.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvmm_parser.so:vendor/lib/libnvmm_parser.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvmm_utils.so:vendor/lib/libnvmm_utils.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvmm_writer.so:vendor/lib/libnvmm_writer.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvmmlite.so:vendor/lib/libnvmmlite.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvmmlite_audio.so:vendor/lib/libnvmmlite_audio.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvmmlite_image.so:vendor/lib/libnvmmlite_image.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvmmlite_msaudio.so:vendor/lib/libnvmmlite_msaudio.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvmmlite_utils.so:vendor/lib/libnvmmlite_utils.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvmmlite_video.so:vendor/lib/libnvmmlite_video.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvodm_imager.so:vendor/lib/libnvodm_imager.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvoice.so:vendor/lib/libnvoice.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvomx.so:vendor/lib/libnvomx.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvomxilclient.so:vendor/lib/libnvomxilclient.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvos.so:vendor/lib/libnvos.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvparser.so:vendor/lib/libnvparser.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvrm.so:vendor/lib/libnvrm.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvrm_gpu.so:vendor/lib/libnvrm_gpu.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvrm_graphics.so:vendor/lib/libnvrm_graphics.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvrmapi_tegra.so:vendor/lib/libnvrmapi_tegra.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvstitching.so:vendor/lib/libnvstitching.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvtnr.so:vendor/lib/libnvtnr.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvtvmr.so:vendor/lib/libnvtvmr.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvvicsi_v3.so:vendor/lib/libnvvicsi_v3.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvwinsys.so:vendor/lib/libnvwinsys.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libnvwsi.so:vendor/lib/libnvwsi.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/liboemcrypto.so:vendor/lib/liboemcrypto.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libopencv24_tegra.so:vendor/lib/libopencv24_tegra.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libphs.so:vendor/lib/libphs.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libpython2.6.so:vendor/lib/libpython2.6.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/librm31080.so:vendor/lib/librm31080.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/librm_ts_service.so:vendor/lib/librm_ts_service.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libscf.so:vendor/lib/libscf.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libstagefrighthw.so:vendor/lib/libstagefrighthw.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libtbb.so:vendor/lib/libtbb.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libtlk_secure_hdcp_up.so:vendor/lib/libtlk_secure_hdcp_up.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libtsec_wrapper.so:vendor/lib/libtsec_wrapper.so \
+    vendor/google/dragon-common/proprietary/vendor/lib/libtsechdcp.so:vendor/lib/libtsechdcp.so \
+    vendor/google/dragon-common/proprietary/vendor/lib64/egl/libEGL_tegra.so:vendor/lib64/egl/libEGL_tegra.so \
+    vendor/google/dragon-common/proprietary/vendor/lib64/egl/libGLESv1_CM_tegra.so:vendor/lib64/egl/libGLESv1_CM_tegra.so \
+    vendor/google/dragon-common/proprietary/vendor/lib64/egl/libGLESv2_tegra.so:vendor/lib64/egl/libGLESv2_tegra.so \
+    vendor/google/dragon-common/proprietary/vendor/lib64/hw/gatekeeper.dragon.so:vendor/lib64/hw/gatekeeper.dragon.so \
+    vendor/google/dragon-common/proprietary/vendor/lib64/hw/gralloc.tegra.so:vendor/lib64/hw/gralloc.tegra.so \
+    vendor/google/dragon-common/proprietary/vendor/lib64/hw/keystore.dragon.so:vendor/lib64/hw/keystore.dragon.so \
+    vendor/google/dragon-common/proprietary/vendor/lib64/hw/memtrack.dragon.so:vendor/lib64/hw/memtrack.dragon.so \
+    vendor/google/dragon-common/proprietary/vendor/lib64/hw/vulkan.tegra.so:vendor/lib64/hw/vulkan.tegra.so \
+    vendor/google/dragon-common/proprietary/vendor/lib64/mediadrm/libwvdrmengine.so:vendor/lib64/mediadrm/libwvdrmengine.so \
+    vendor/google/dragon-common/proprietary/vendor/lib64/libdrm.so:vendor/lib64/libdrm.so \
+    vendor/google/dragon-common/proprietary/vendor/lib64/libglcore.so:vendor/lib64/libglcore.so \
+    vendor/google/dragon-common/proprietary/vendor/lib64/libnvavp.so:vendor/lib64/libnvavp.so \
+    vendor/google/dragon-common/proprietary/vendor/lib64/libnvblit.so:vendor/lib64/libnvblit.so \
+    vendor/google/dragon-common/proprietary/vendor/lib64/libnvcms.so:vendor/lib64/libnvcms.so \
+    vendor/google/dragon-common/proprietary/vendor/lib64/libnvcompute.so:vendor/lib64/libnvcompute.so \
+    vendor/google/dragon-common/proprietary/vendor/lib64/libnvddk_2d_v2.so:vendor/lib64/libnvddk_2d_v2.so \
+    vendor/google/dragon-common/proprietary/vendor/lib64/libnvddk_vic.so:vendor/lib64/libnvddk_vic.so \
+    vendor/google/dragon-common/proprietary/vendor/lib64/libnvglsi.so:vendor/lib64/libnvglsi.so \
+    vendor/google/dragon-common/proprietary/vendor/lib64/libnvgr.so:vendor/lib64/libnvgr.so \
+    vendor/google/dragon-common/proprietary/vendor/lib64/libnvos.so:vendor/lib64/libnvos.so \
+    vendor/google/dragon-common/proprietary/vendor/lib64/libnvrm.so:vendor/lib64/libnvrm.so \
+    vendor/google/dragon-common/proprietary/vendor/lib64/libnvrm_gpu.so:vendor/lib64/libnvrm_gpu.so \
+    vendor/google/dragon-common/proprietary/vendor/lib64/libnvrm_graphics.so:vendor/lib64/libnvrm_graphics.so \
+    vendor/google/dragon-common/proprietary/vendor/lib64/libnvrmapi_tegra.so:vendor/lib64/libnvrmapi_tegra.so \
+    vendor/google/dragon-common/proprietary/vendor/lib64/libnvwinsys.so:vendor/lib64/libnvwinsys.so \
+    vendor/google/dragon-common/proprietary/vendor/lib64/libnvwsi.so:vendor/lib64/libnvwsi.so \
+    vendor/google/dragon-common/proprietary/vendor/lib64/libphs.so:vendor/lib64/libphs.so \
+    vendor/google/dragon-common/proprietary/vendor/lib64/libstagefrighthw.so:vendor/lib64/libstagefrighthw.so \
+    vendor/google/dragon-common/proprietary/vendor/media/LMspeed_508.emd:vendor/media/LMspeed_508.emd \
+    vendor/google/dragon-common/proprietary/vendor/media/PFFprec_600.emd:vendor/media/PFFprec_600.emd
---
+++ dragon/device-vendor.mk
@@ -0,0 +1,31 @@
+# Copyright 2015-2018 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Prebuilt APKs
+PRODUCT_PACKAGES += \
+    UserInfoWidget
+
+# Prebuilt privileged APKs
+PRODUCT_PACKAGES += \
+    DragonKeyboardFirmwareUpdater \
+    GCS \
+    HotwordEnrollmentOKGoogleRT5677
+
+# Nvidia blob(s) necessary for Dragon hardware
+PRODUCT_COPY_FILES := \
+    vendor/google/dragon/proprietary/etc/permissions/privapp-permissions-google.xml:system/etc/permissions/privapp-permissions-google.xml \
+    vendor/google/dragon/proprietary/etc/permissions/privapp-permissions-platform.xml:system/etc/permissions/privapp-permissions-platform.xml \
+    vendor/google/dragon/proprietary/etc/sysconfig/nexus.xml:system/etc/sysconfig/nexus.xml \
+    vendor/google/dragon/proprietary/lib/libkeymaster_staging.so:system/lib/libkeymaster_staging.so \
+    vendor/google/dragon/proprietary/lib64/libkeymaster_staging.so:system/lib64/libkeymaster_staging.so
